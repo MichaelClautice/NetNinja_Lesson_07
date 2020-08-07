2e2bb0d68039fa02db723221aa15ab042316db79
Michael Clautice <michaelclautice@Michaels-MacBook-Pro-2.local>
2020-08-07 17:08:33 -0400

    Mike changed 2 files F8.7.20

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-var stuff = require('./stuff.js');
+// app.js is the main-entry file for this app
+// this file - app.js, is what we run to run the app
+// req the custom module stuff.js
+let stuff = require('./stuff.js');
 
+// console.log this:
+// call mikesCounter fnctn in stuff.js 
+// send it an array arg
 console.log(stuff.mikesCounter(['arrItem1', 'arrItem2', 'arrItem3', 'arrItem4', 'arrItem5', 'arrItem6']));
 
+// console.log this:
+// call mikesAdder fnctn in stuff.js 
+// send it 2 int args
 console.log(stuff.mikesAdder(11,9));
 
+// console.log this:
+// call mikesAdder fnctn in stuff.js 
+// send it th valu of MikesPi & an int
 console.log(stuff.mikesAdder(stuff.mikesPi, 2));
--- stuff.js
+++ stuff.js
@@ -1,12 +1,12 @@
-var mikesCounter = function(arr){
-    return '•••••••••\nThere be ' + arr.length + ' elements in Mikes demo array';
+let mikesCounter = function(arr){
+    return '•••••••••\nmikesCounter:\nThere be ' + arr.length + ' elements in Mikes demo array';
 };
 
-var mikesAdder = function(a,b){
-    return `•••••••••\nDa sum of da 2 numbers is ${a+b}\n•••••••••`;
+let mikesAdder = function(a,b){
+    return `•••••••••\nmikesAdder:\nDa sum of da 2 numbers is ${a+b}\n•••••••••`;
 };
 
-var mikesPi = 3.412;
+let mikesPi = 3.412;
 
 // module.exports.mikesCounter = mikesCounter;
 // module.exports.mikesAdder = mikesAdder;
